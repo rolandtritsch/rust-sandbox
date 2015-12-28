4786ce8c549ce4af5a0ea7bd292c35b8ebf07aa7
Roland Tritsch <roland.tritsch@gonitro.com>
2015-12-28 22:21:12 +0000

    Hello World 2.0

--- hello/src/hello.rs
+++ hello/src/hello.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello World");
+    let mut text = vec!["Hello", "World"];
+    {
+        let hello = &text[0];
+        let world = &text[1];
+        println!("{} {}", hello, world);
+    }
+    text.push("Roland");
+    println!("{} {} {}", text[0], text[1], text[2]);
 }
