e5c8f73cf7928bbaf4598f4453d33ac884bd32d3
Roland Tritsch <roland.tritsch@gonitro.com>
2015-12-29 07:47:23 +0000

    guess: Add command line processing

--- guess/src/guess.rs
+++ guess/src/guess.rs
@@ -2,11 +2,25 @@ extern crate rand;
 
 use rand::Rng;
 use std::cmp::Ordering;
+use std::env;
 use std::io;
 
 fn main() {
+    let args: Vec<_> = env::args().collect();
+    let cmd = args[0].to_string();
+    let max: u32;
+    match args.len() {
+        2 => {
+            max = args[1].to_string().trim().parse().ok().expect("Failed to parse <max>");
+        }
+        _ => {
+            println!("Usage: {} <max>", cmd);
+            return;
+        }
+    }
+
     println!("!!! Welcome to ... Guess the number !!!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, max + 1);
 
     loop {
         println!("Please input your guess: ");
